2aeba3e695ad6cf5ddb23a233d801995a7af1cfa
Ricardo Note-R <rfiori@users.noreply.github.com>
2023-07-14 15:34:51 -0300

    Separete JS function in new file JS

---
+++ src/MKTListNet/wwwroot/js/site_ChechBox.js
@@ -0,0 +1,16 @@
+﻿function GetCheckBoxes(data_Tag) {
+    var tag = 'input[type="checkbox"][' + data_Tag + ']'
+    return document.querySelectorAll(tag);
+}
+
+function checkAll(checkbox) {
+    var checkboxes = GetCheckBoxes("data-check_n");
+
+    // Check all checkboxes, except self was clicked.
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] !== checkbox) {
+            checkboxes[i].checked = checkbox.checked;
+        }
+    }
+}
+
